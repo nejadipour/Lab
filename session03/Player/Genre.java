39c6d0332f646fe853eaa79e311cb3328e19c364
Alireza Nejadipour <78561069+nejadipour@users.noreply.github.com>
2021-03-13 18:30:17 +0330

    feat: add class Genre

---
+++ session03/Player/Genre.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+
+/**
+ * A class to hold details of each genre
+ *
+ * @author Alireza Nejadipour
+ * @version 2021.Mar.13
+ */
+
+public class Genre
+{
+    private String name;
+    private ArrayList<Music> musics;
+    private ArrayList<Artist> artists;
+}
