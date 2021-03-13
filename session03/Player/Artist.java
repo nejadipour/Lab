b875ad3b548d5bd86c260ec982ef08d6134fe0c4
Alireza Nejadipour <78561069+nejadipour@users.noreply.github.com>
2021-03-13 18:29:50 +0330

    feat: add class Artist

---
+++ session03/Player/Artist.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+
+/**
+ * A class to hold details of each artist
+ *
+ * @author Alireza Nejadipour
+ * @version 2021.Mar.13
+ */
+
+public class Artist
+{
+    private String name;
+    private ArrayList<Music> musics;
+}
