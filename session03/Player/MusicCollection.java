efeb2116b379a00f0db84f4696de898d9085999d
Alireza Nejadipour <78561069+nejadipour@users.noreply.github.com>
2021-03-23 11:22:36 +0430

    feat: addMusic is completed somhow

--- session03/Player/MusicCollection.java
+++ session03/Player/MusicCollection.java
@@ -1,52 +1,96 @@
-import java.util.ArrayList;
-import java.util.Iterator;
+
 import java.util.Scanner;
 
-/**
- * A class to hold details of audio files.
- *
- * @author Alireza Nejadipour
- * @version 2021.Mar.13
- */
-
-public class MusicCollection
+public class Run
 {
-    private ArrayList<Artist> artists;
-    private ArrayList<Music> musics;
-    private ArrayList<Genre> genres;
-    
+    private String mainMenu;
+    private String musicsMenu;
+    private int choice;
+    private Scanner scanner;
+    private MusicCollection runtimeMusicCollection;
 
-    public MusicCollection()
+    public Run()
     {
-        artists = new ArrayList<>();
-        musics = new ArrayList<>();
-        genres = new ArrayList<>();
+        runtimeMusicCollection = new MusicCollection();
 
-        
+        scanner = new Scanner(System.in).useDelimiter("\n");
+
+        mainMenu =
+                "1.Musics\n" +
+                        "2.Artists\n" +
+                        "3.Genres\n";
+
+        musicsMenu =
+                "1.Show musics\n" +
+                        "2.Add music\n" +
+                        "3.Remove music\n" +
+                        "4.Main menu\n";
+
+    }
+
+    public void userChoice()
+    {
+        System.out.print("Your Choice : ");
+        choice = scanner.nextInt();
 
     }
 
 
-    public boolean displayMusics()
+    public void printManiMenu()
     {
-        int musicNum = 1;
+        System.out.println(mainMenu);
+        userChoice();
 
-        if (musics.size() == 0)
+        switch (choice)
         {
-            System.out.println("No music found.");
-            return false;
+            case 1:
+                printMusicsMenu();
+                break;
+            case 2:
+                break;
 
+            case 3:
+                break;
+
+            case 4:
+                break;
         }
-        else
+    }
+
+
+    public void printMusicsMenu()
+    {
+        System.out.println(musicsMenu);
+
+        userChoice();
+
+        switch (choice)
         {
-            for (Music music : musics)
-            {
-                System.out.println("Music " + musicNum + ":");
-                music.print();
+            case 1:
+                if(runtimeMusicCollection.displayMusics())
+                {
+                    userChoice();
+                    // TODO: 3/23/2021 play the music by its address
+                }
+                else
+                {
+                    printMusicsMenu();
+                }
+                break;
 
-            }
+            case 2:
+                addMusic();
+                printMusicsMenu();
+                break;
 
-            return true;
+            case 3:
+                //runtimeMusicCollection.removeMusic();
+                printManiMenu();
+                break;
+
+            case 4:
+                printManiMenu();
+                break;
 
         }
     }
@@ -54,20 +98,33 @@ public class MusicCollection
 
     public void addMusic()
     {
+        System.out.print("Enter the name : ");
+        String name = scanner.next();
         
-        
-        
+        System.out.print("Enter the artist : ");
+        String artistName = scanner.next();
 
+        System.out.print("Enter the genre : ");
+        String genre = scanner.next();
+
+        System.out.print("Enter the address : ");
+        String address = scanner.next();
+
+        System.out.print("Enter the year : ");
+        int year = scanner.nextInt();
+
+        Music musicToAdd = new Music(name, genre, artistName, address, year);
+
+        runtimeMusicCollection.addMusic(musicToAdd);
+        
     }
 
 
-    public void removeMusic()
+    public static void main(String[] args)
     {
-        System.out.println("Enter the address of music : ");
-        String address;
-        
-        
+        Run run = new Run();
 
+        run.printManiMenu();
     }
 
 
