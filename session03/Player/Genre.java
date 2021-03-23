6213df972c0a695085f8dd40b4d817eb0e2c1cd1
Alireza Nejadipour <78561069+nejadipour@users.noreply.github.com>
2021-03-23 10:13:26 +0430

    feat: constructor and methods added
    method addMusic and findMusic added

--- session03/Player/Genre.java
+++ session03/Player/Genre.java
@@ -12,4 +12,47 @@ public class Genre
     private String name;
     private ArrayList<Music> musics;
     private ArrayList<Artist> artists;
+
+    public Genre(String name)
+    {
+        this.name = name;
+        musics = new ArrayList<>();
+        artists = new ArrayList<>();
+    }
+
+
+    public void addMusic(Music music)
+    {
+        if (findMusic(music.getName()) == null)
+        {
+            musics.add(music);
+
+        }
+        else
+        {
+            System.out.println("Music is already available.");
+
+        }
+
+    }
+
+
+    public Music findMusic(String name)
+    {
+        for (Music music : musics)
+        {
+            if (music.getName().equals(name))
+            {
+                return music;
+            }
+
+        }
+
+        return null;
+    }
+
+    public void print()
+    {
+
+    }
 }
