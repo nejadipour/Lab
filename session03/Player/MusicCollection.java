79ad2b7d9a40d9e63030aa619ffb25fe698943f7
Alireza Nejadipour <78561069+nejadipour@users.noreply.github.com>
2021-03-23 10:25:43 +0430

    fix: write again!
    i decided to start writnig this class from 0

--- session03/Player/MusicCollection.java
+++ session03/Player/MusicCollection.java
@@ -10,124 +10,18 @@ import java.util.Iterator;
 
 public class MusicCollection
 {
-    // An ArrayList for storing the file names of music files.
-    private ArrayList<Music> files;
-    // A player for the music files.
-    private MusicPlayer player;
-
-    /**
-     * Create a MusicCollection
-     */
+    private ArrayList<Artist> artists;
+    private ArrayList<Music> musics;
+    private ArrayList<Genre> genres;
+    
     public MusicCollection()
     {
-
-
+        artists = new ArrayList<>();
+        musics = new ArrayList<>();
+        genres = new ArrayList<>();
+        
     }
-
-    /**
-     * Add a file to the collection.
-     * @param filename The file to be added.
-     */
-    public void addFile(Music filename)
-    {
-
-    }
-
-    /**
-     * Return the number of files in the collection.
-     * @return The number of files in the collection.
-     */
-    public int getNumberOfFiles()
-    {
-        return files.size();
-    }
-
-    /**
-     * List a file from the collection.
-     * @param genre The index of the file to be listed.
-     */
-    public void listFile(String genre)
-    {
-        int index = 0;
-
-        while (index < files.size())
-        {
-            String currentGenre = files.get(index).getGenre();
-
-            if (currentGenre.contains(genre))
-            {
-                files.get(index).print();
-                break;
-            }
-            else
-            {
-                index++;
-            }
-
-        }
-
-    }
-
-    /**
-     * Show a list of all the files in the collection.
-     */
-    public void listAllFiles()
-    {
-        for(Music file : files)
-        {
-            file.print();
-            System.out.println("__________________________");
-        }
-
-    }
-
-    /**
-     * Remove a file from the collection.
-     * @param name The index of the file to be removed.
-     */
-    public void removeFile(String name)
-    {
-        Iterator<Music> it = files.iterator();
-        while(it.hasNext())
-        {
-            Music music = it.next();
-            if(name.contains(music.getName()))
-            {
-                it.remove();
-            }
-        }
-
-    }
-
-    /**
-     * Start playing a file in the collection.
-     * Use stopPlaying() to stop it playing.
-     * @param index The index of the file to be played.
-     */
-    public void startPlaying(int index)
-    {
-
-    }
-
-    /**
-     * Stop the player.
-     */
-    public void stopPlaying()
-    {
-
-    }
-
-
-    /**
-     * Determine whether the given index is valid for the collection.
-     * Print an error message if it is not.
-     * @param index The index to be checked.
-     * @return true if the index is valid, false otherwise.
-     */
-//    private boolean validIndex(int index)
-//    {
-//        // The return value.
-//        // Set according to whether the index is valid or not.
-//
-//    }
+    
+    
+    
 }
