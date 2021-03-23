2e1d49d2e8e117369beb6b11a9893881c25b917e
Alireza Nejadipour <78561069+nejadipour@users.noreply.github.com>
2021-03-23 10:43:27 +0430

    feat: some menus and methods related to them added

--- session03/Player/MusicCollection.java
+++ session03/Player/MusicCollection.java
@@ -15,6 +15,7 @@ public class MusicCollection
     private ArrayList<Music> musics;
     private ArrayList<Genre> genres;
     private String mainMenu;
+    private String musicsMenu;
     private int choice;
     private Scanner scanner;
 
@@ -23,32 +24,127 @@ public class MusicCollection
         artists = new ArrayList<>();
         musics = new ArrayList<>();
         genres = new ArrayList<>();
-        
+
         scanner = new Scanner(System.in);
-        
-        mainMenu = 
+
+        mainMenu =
                 "1.Musics\n" +
                 "2.Artists\n" +
-                "3.Genres\n" +
-                "4.Add music\n";
+                "3.Genres\n";
+        
+        musicsMenu =
+                "1.Show musics\n" +
+                "2.Add music\n" +
+                "3.Remove music\n" +
+                "4.Main menu\n";
 
     }
-    
-    
+
+
     public void userChoice()
     {
         System.out.print("Your Choice : ");
         choice = scanner.nextInt();
-        
+
     }
-    
-    
+
+
     public void printManiMenu()
     {
         System.out.println(mainMenu);
         userChoice();
+        
+        switch (choice)
+        {
+            case 1:
+                printMusicsMenu();
+                break;
+            case 2:
+                break;
+                
+            case 3:
+                break;
+                
+            case 4:
+                break;
+        }
+    }
+    
+    
+    public void printMusicsMenu()
+    {
+        System.out.println(musicsMenu);
+        
+        userChoice();
+        
+        switch (choice)
+        {
+            case 1:
+                if(displayMusics())
+                {
+                    userChoice();
+                    // TODO: 3/23/2021 play the music by its address 
+                }
+                else
+                {
+                    printMusicsMenu();
+                }
+                break;
+                
+            case 2:
+                addMusic();
+                printMusicsMenu();
+                break;
+                
+            case 3:
+                removeMusic();
+                printManiMenu();
+                break;
+                
+            case 4:
+                printManiMenu();
+                break;
+                
+        }
+    }
+    
+    
+    public boolean displayMusics()
+    {
+        int musicNum = 1;
+        
+        if (musics.size() == 0)
+        {
+            System.out.println("No music found.");
+            return false;
+            
+        }
+        else
+        {
+            for (Music music : musics)
+            {
+                System.out.println("Music " + musicNum + ":");
+                music.print();
+
+            }
+            
+            return true;
+
+        }
+    }
+    
+    
+    public void addMusic()
+    {
+        
+    }
+    
+    
+    public void removeMusic()
+    {
+        
     }
 
-    
+
 
 }
