3401e76d97dd677683d8a95f939e11d80fd6097a
Alireza Nejadipour <78561069+nejadipour@users.noreply.github.com>
2021-03-23 10:31:13 +0430

    feat: variables updated and methods added

--- session03/Player/MusicCollection.java
+++ session03/Player/MusicCollection.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.Scanner;
 
 /**
  * A class to hold details of audio files.
@@ -13,15 +14,41 @@ public class MusicCollection
     private ArrayList<Artist> artists;
     private ArrayList<Music> musics;
     private ArrayList<Genre> genres;
-    
+    private String mainMenu;
+    private int choice;
+    private Scanner scanner;
+
     public MusicCollection()
     {
         artists = new ArrayList<>();
         musics = new ArrayList<>();
         genres = new ArrayList<>();
         
+        scanner = new Scanner(System.in);
+        
+        mainMenu = 
+                "1.Musics\n" +
+                "2.Artists\n" +
+                "3.Genres\n" +
+                "4.Add music\n";
+
     }
     
     
+    public void userChoice()
+    {
+        System.out.print("Your Choice : ");
+        choice = scanner.nextInt();
+        
+    }
     
+    
+    public void printManiMenu()
+    {
+        System.out.println(mainMenu);
+        userChoice();
+    }
+
+    
+
 }
