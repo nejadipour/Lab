8af7d88a8b11bac9d1fb791c9aa0ce03a350ce4f
Alireza Nejadipour <78561069+nejadipour@users.noreply.github.com>
2021-03-24 12:48:55 +0430

    feat: when u remove an artist the songs remove too

--- session03/Player/Artist.java
+++ session03/Player/Artist.java
@@ -1,67 +1,33 @@
+import com.sun.source.tree.ArrayAccessTree;
+
 import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Scanner;
 
 /**
- * A class to hold details of each artist
+ * A class to hold details of audio files.
  *
  * @author Alireza Nejadipour
  * @version 2021.Mar.13
  */
 
-public class Artist
+public class MusicCollection
 {
-    private String name;
+    private ArrayList<Artist> artists;
     private ArrayList<Music> musics;
-    private Genre genre;
+    private ArrayList<Genre> genres;
 
-    public Artist(String name)
+
+    public MusicCollection()
     {
-        this.name = name;
+        artists = new ArrayList<>();
         musics = new ArrayList<>();
+        genres = new ArrayList<>();
 
     }
 
 
-    public void addMusic(Music music)
-    {
-        if (findMusic(music.getName()) == null)
-        {
-            musics.add(music);
-
-        }
-
-    }
-
-
-    public Music findMusic(String name)
-    {
-        for (Music music : musics)
-        {
-            if (music.getName().equals(name))
-            {
-                return music;
-
-            }
-
-        }
-
-        return null;
-    }
-    
-    
-    public void removeMusic(Music music)
-    {
-        musics.remove(music);
-        
-    }
-
-
-    public void print()
-    {
-        System.out.println(getName() + " | " + getMusics().size() + " musics");
-    }
-
-
-    public boolean printMusics()
+    public boolean displayMusics()
     {
         int musicNum = 1;
 
@@ -78,7 +44,94 @@ public class Artist
                 System.out.println("Music " + musicNum + ":");
                 music.print();
 
+            musicNum++;
+
             }
+
+            return true;
+
+        }
+    }
+
+
+    public boolean addMusic(Music music)
+    {
+        if (findMusic(music.getAddress()) == null)
+        {
+            musics.add(music);
+
+            String artistName = music.getArtist();
+            Artist artist = findArtist(artistName);
+            if (artist == null)
+            {
+                artist = new Artist(artistName);
+                artists.add(artist);
+            }
+            artist.addMusic(music);
+
+            String genreName = music.getGenre();
+            Genre genre = findGenre(genreName);
+            if (genre == null)
+            {
+                genre = new Genre(genreName);
+                genres.add(genre);
+            }
+            genre.addMusic(music);
+
+            return true;
+
+
+        }
+        else
+        {
+            return false;
+
+        }
+
+    }
+
+
+    public Music findMusic(String address)
+    {
+        for (Music music : musics)
+        {
+            if (music.getAddress().equals(address))
+            {
+                return music;
+
+            }
+
+        }
+
+        return null;
+    }
+
+
+    public boolean removeMusic(String address)
+    {
+        System.out.println("here1");
+        Music musicToRemove = findMusic(address);
+        System.out.println("here3");
+
+        if (musicToRemove == null)
+        {
+            return false;
+        }
+        else
+        {
+            System.out.println("here4");
+            musics.remove(musicToRemove);
+            System.out.println("here5");
+
+            Artist artist = findArtist(musicToRemove.getArtist());
+            System.out.println("here6");
+            artist.removeMusic(musicToRemove);
+            System.out.println("here7");
+
+            Genre genre = findGenre(musicToRemove.getGenre());
+            System.out.println("here8");
+            genre.removeMusic(musicToRemove);
+            System.out.println("here9");
 
             return true;
 
@@ -87,14 +140,187 @@ public class Artist
     }
 
 
-    public String getName()
+    public boolean displayArtists()
     {
-        return name;
+        int artistNum = 1;
+
+        if (artists.size() == 0)
+        {
+            System.out.println("No artist found.");
+            return false;
+        }
+        else
+        {
+            for(Artist artist : artists)
+            {
+                System.out.println("Artist " + artistNum + ":");
+                artist.print();
+
+                artistNum++;
+
+            }
+
+        }
+
+        return true;
+
     }
 
-    public void setName(String name)
+
+    public boolean addArtist(Artist artist)
     {
-        this.name = name;
+        if (findArtist(artist.getName()) == null)
+        {
+            artists.add(artist);
+            return true;
+
+        }
+        else
+        {
+            return false;
+
+        }
+
+    }
+
+
+    public Artist findArtist(String name)
+    {
+        for(Artist artist : artists)
+        {
+            if(artist.getName().equals(name))
+            {
+                return artist;
+
+            }
+        }
+
+        return null;
+
+    }
+
+
+    public void removeArtist(String name)
+    {
+        Artist artistToRemove = findArtist(name);
+
+        if (artistToRemove == null)
+        {
+            System.out.println("Artist doesn't exist.");
+
+        }
+        else
+        {
+            ArrayList<Music> musicsToRemove = artistToRemove.getMusics();
+
+            while(musicsToRemove.size() != 0)
+            {
+                Music music = musicsToRemove.get(0);
+                removeMusic(music.getAddress());
+                musicsToRemove.remove(music);
+
+            }
+
+            artists.remove(artistToRemove);
+
+            System.out.println("Artist removed.");
+
+        }
+
+    }
+
+
+    public boolean displayGenres()
+    {
+        int genreNum = 1;
+
+        if (genres.size() == 0)
+        {
+            System.out.println("No genre found.");
+            return false;
+        }
+        else
+        {
+            for(Genre genre : genres)
+            {
+                System.out.println("Genre " + genreNum + ":");
+                genre.print();
+
+                genreNum++;
+
+            }
+
+        }
+
+        return true;
+
+
+    }
+
+
+    public boolean addGenre(Genre genre)
+    {
+        if (findGenre(genre.getName()) == null)
+        {
+            genres.add(genre);
+            return true;
+
+
+        }
+        else
+        {
+            return false;
+
+        }
+
+    }
+
+
+    public Genre findGenre(String name)
+    {
+        for(Genre genre : genres)
+        {
+            if(genre.getName().equals(name))
+            {
+                return genre;
+
+            }
+        }
+
+        return null;
+
+    }
+
+
+    public void removeGenre(String name)
+    {
+        Genre genreToRemove = findGenre(name);
+
+        if (genreToRemove == null)
+        {
+            System.out.println("Genre doesn't exist.");
+
+        }
+        else
+        {
+            genres.remove(genreToRemove);
+            System.out.println("Genre removed.");
+
+        }
+
+    }
+
+
+
+
+    public ArrayList<Artist> getArtists()
+    {
+        return artists;
+    }
+
+    public void setArtists(ArrayList<Artist> artists)
+    {
+        this.artists = artists;
     }
 
     public ArrayList<Music> getMusics()
@@ -107,13 +333,13 @@ public class Artist
         this.musics = musics;
     }
 
-    public Genre getGenre()
+    public ArrayList<Genre> getGenres()
     {
-        return genre;
+        return genres;
     }
 
-    public void setGenre(Genre genre)
+    public void setGenres(ArrayList<Genre> genres)
     {
-        this.genre = genre;
+        this.genres = genres;
     }
 }
