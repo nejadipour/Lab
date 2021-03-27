95804f9df6437e9c9307a03553f440f243d41e92
Alireza Nejadipour <78561069+nejadipour@users.noreply.github.com>
2021-03-27 18:15:23 +0430

    refactor: code clean and Javadoc added

--- session03/Player/Artist.java
+++ session03/Player/Artist.java
@@ -4,15 +4,18 @@ import java.util.ArrayList;
  * A class to hold details of each artist
  *
  * @author Alireza Nejadipour
- * @version 2021.Mar.13
+ * @version 4 (3/26/2021)
  */
 
 public class Artist
 {
-    private String name;
-    private ArrayList<Music> musics;
-    private Genre genre;
+    private final String name;
+    private final ArrayList<Music> musics;
 
+    /**
+     * create a new artist with the given name
+     * @param name the name of artist
+     */
     public Artist(String name)
     {
         this.name = name;
@@ -21,6 +24,10 @@ public class Artist
     }
 
 
+    /**
+     * adds a new music to the list of musics
+     * @param music the music to add
+     */
     public void addMusic(Music music)
     {
         if (findMusic(music.getName()) == null)
@@ -32,6 +39,11 @@ public class Artist
     }
 
 
+    /**
+     * checks the given music to see if it is new or not
+     * @param name the name of music to be checked
+     * @return if the music is found returns the music, else returns null
+     */
     public Music findMusic(String name)
     {
         for (Music music : musics)
@@ -48,6 +60,10 @@ public class Artist
     }
 
 
+    /**
+     * removes a music from the list
+     * @param music the music to be removed
+     */
     public void removeMusic(Music music)
     {
         musics.remove(music);
@@ -55,12 +71,20 @@ public class Artist
     }
 
 
+    /**
+     * prints the data of artist
+     * like the name and the number of musics by the artist
+     */
     public void print()
     {
         System.out.println(getName() + " | " + getMusics().size() + " musics");
     }
 
 
+    /**
+     * prints all the songs of the artist
+     * @return false if no music is available
+     */
     public boolean printMusics()
     {
         int musicNum = 1;
@@ -87,33 +111,22 @@ public class Artist
     }
 
 
+    /**
+     * gets the name of artist
+     * @return name field
+     */
     public String getName()
     {
         return name;
     }
 
-    public void setName(String name)
-    {
-        this.name = name;
-    }
-
+    /**
+     * gets the list of musics by the artist
+     * @return musics field is returned
+     */
     public ArrayList<Music> getMusics()
     {
         return musics;
     }
 
-    public void setMusics(ArrayList<Music> musics)
-    {
-        this.musics = musics;
-    }
-
-    public Genre getGenre()
-    {
-        return genre;
-    }
-
-    public void setGenre(Genre genre)
-    {
-        this.genre = genre;
-    }
 }
