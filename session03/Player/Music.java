9d49923aebf87f8035960f921b5bfceb1bc09299
Alireza Nejadipour <78561069+nejadipour@users.noreply.github.com>
2021-03-27 18:15:55 +0430

    refactor: code cleaned and Javadoc added

--- session03/Player/Music.java
+++ session03/Player/Music.java
@@ -2,18 +2,25 @@
  * A class to hold details of each music
  *
  * @author Alireza Nejadipour
- * @version 2021.Mar.13
+ * @version 2 (3/26/2021)
  */
-
 
 public class Music
 {
-    private String name;
-    private String genre;
-    private String artist;
-    private String address;
-    private int year;
+    private final String name;
+    private final String genre;
+    private final String artist;
+    private final String address;
+    private final int year;
 
+    /**
+     * create a new music by given parameters
+     * @param name the name of music
+     * @param genre the genre of the new music
+     * @param artist the name of the artist of the music
+     * @param address the file path of where music is located
+     * @param year the year when music was released
+     */
     public Music(String name, String genre, String artist, String address, int year)
     {
         this.name = name;
@@ -21,62 +28,64 @@ public class Music
         this.artist = artist;
         this.address = address;
         this.year = year;
-        
+
     }
 
+
+    /**
+     * prints the data of music
+     * like name, artist, genre and year
+     */
     public void print()
     {
+        System.out.println(getName() + " | " + getArtist() + " | " + getGenre() + " | " + getYear() + "\n");
 
     }
 
 
+    /**
+     * gets the year when the music was released
+     * @return the year field is returned
+     */
     public int getYear()
     {
         return year;
     }
 
+    /**
+     * the address of where music is located is returned
+     * @return the address field
+     */
     public String getAddress()
     {
         return address;
     }
 
+    /**
+     * gets the name of the artist of the music
+     * @return the field artist is returned
+     */
     public String getArtist()
     {
         return artist;
     }
 
+    /**
+     * gets the music's genre's name
+     * @return the genre field is returned
+     */
     public String getGenre()
     {
         return genre;
     }
 
+    /**
+     * gets the name of the music
+     * @return the name field
+     */
     public String getName()
     {
         return name;
     }
 
-    public void setAddress(String address)
-    {
-        this.address = address;
-    }
-
-    public void setArtist(String artist)
-    {
-        this.artist = artist;
-    }
-
-    public void setGenre(String genere)
-    {
-        this.genre = genere;
-    }
-
-    public void setName(String name)
-    {
-        this.name = name;
-    }
-
-    public void setYear(int year)
-    {
-        this.year = year;
-    }
 }
