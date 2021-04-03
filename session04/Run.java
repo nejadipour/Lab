c83ef3ca98f7611ba5f57858bc32c4a4107e30c5
Alireza Nejadipour <78561069+nejadipour@users.noreply.github.com>
2021-04-03 18:04:40 +0430

    feat: add newVoting method

--- session04/Run.java
+++ session04/Run.java
@@ -1,28 +1,37 @@
+import java.util.Scanner;
+
 public class Main
 {
     private int choice;
     private Scanner scanner;
-    public String mainMeu;
-    
-    
+    private String mainMeu;
+    private String typeMenu;
+
+
     public Main()
     {
         mainMeu =
                 """
-                1.new voting
-                2.results
-                3.show all voting
-                """;
-        
+                    1.new voting
+                    2.results
+                    3.show all voting
+                        """;
+
+        typeMenu =
+                """
+                        1.single vote
+                        2.multiple vote
+                        """;
+
         scanner = new Scanner(System.in).useDelimiter("\n");
-        
+
     }
-    
-    
+
+
     public void printMainMenu()
     {
         System.out.println(mainMeu);
-        
+
         userChoice();
 
         switch (choice)
@@ -31,35 +40,61 @@ public class Main
             case 2 -> results();
         }
     }
-    
-    
-    
+
+
+
     public void userChoice()
     {
         System.out.print("Your Choice : ");
         choice = scanner.nextInt();
-        
+
     }
-    
-    
+
+
     public void new_voting()
     {
+        System.out.print("Enter the question : ");
+        String question = scanner.next();
         
+        System.out.println("Enter the questions : ");
+        System.out.println("(when it is finished enter \"finish\")");
+        
+        ArrayList<String>choices = new ArrayList<>();
+        String choiceToAdd;
+        while (true)
+        {
+            choiceToAdd = scanner.next();
+
+            if (choiceToAdd.equals("finish"))
+                break;
+
+            choices.add(choiceToAdd);
+
+        }
+
+        System.out.println("Choose your type :");
+        System.out.println(typeMenu);
+        int type = scanner.nextInt() - 1;
+        
+
     }
-    
-    
+
+
     public void results()
     {
-        
+
     }
-    
-    
-    
-    
+
+
+
+
 
     public static void main(String[] args)
     {
 
+        Main run = new Main();
+
+        run.printMainMenu();
 
     }
 }
