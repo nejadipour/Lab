6741e5c7c170203c7a4eb3797bb53d227f9865e8
Alireza Nejadipour <78561069+nejadipour@users.noreply.github.com>
2021-04-03 18:47:10 +0430

    feat: methods updated
    printVoting and printVotingQuestions updated

--- session04/VotingSystem.java
+++ session04/VotingSystem.java
@@ -1,8 +1,11 @@
+//import ir.huri.jcal.*;
+
 import java.util.ArrayList;
 
 public class VotingSystem
 {
     private ArrayList<Voting> votingList;
+
 
     public VotingSystem()
     {
@@ -14,29 +17,50 @@ public class VotingSystem
     public void createVoting(String question, int type, ArrayList<String> choices)
     {
         Voting newVoting = new Voting(type, question);
+        votingList.add(newVoting);
 
     }
 
 
     public void printVotingQuestions()
     {
+        int votingNum = 1;
         for (Voting voting : votingList)
         {
+            System.out.println("Voting " + votingNum + ":");
             System.out.println(voting.getQuestion());
+
+            votingNum++;
 
         }
 
     }
 
 
-    public void printVoting()
+    public void printVoting(int votingNum)
     {
+        Voting voting = votingList.get(votingNum - 1);
+        System.out.println(voting.getQuestion());
+
+        ArrayList<String> choices = voting.getChoices();
+
+        int choiceNum = 1;
+
+        for (String choice : choices)
+        {
+            System.out.print(choiceNum + ".");
+            System.out.println(choice);
+
+            choiceNum++;
+            
+        }
 
     }
 
 
     public void vote(int votingNum, Person person, ArrayList<String> choices)
     {
+        
 
     }
 
