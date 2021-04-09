a7dd468eed759b93fd9558329f5c96699319153c
Alireza Nejadipour <78561069+nejadipour@users.noreply.github.com>
2021-04-09 22:58:37 +0430

    refactor: code cleaned and Javadoc added

--- session04/Vote.java
+++ session04/Vote.java
@@ -1,8 +1,19 @@
+/**
+ * vote's information is stored in this class
+ * @version 1.6
+ * @author Alireza Nejadipour
+ */
+
 public class Vote
 {
-    private Person person;
-    private String date;
+    private final Person person;
+    private final String date;
 
+    /**
+     * create a new vote with passed parameters
+     * @param person the person who has voted
+     * @param date the date the vote is being added
+     */
     public Vote(Person person, String date)
     {
         this.person = person;
@@ -11,8 +22,11 @@ public class Vote
     }
 
 
-
-
+    /**
+     * checks if two votes are equal or not
+     * @param vote the vote that should be checked
+     * @return if they are same returns true
+     */
     public boolean equals(Vote vote)
     {
         return this.date.equals(vote.getDate()) && this.person.equals(vote.getPerson());
@@ -20,19 +34,24 @@ public class Vote
     }
 
 
-//    public int hashCode()
-//    {
-//
-//    }
-
-
+    /**
+     * gets the person who has voted
+     * @return field person is returned
+     */
     public Person getPerson()
     {
         return person;
+
     }
 
+    /**
+     * gets the date person has voted
+     * @return field date is returned
+     */
     public String getDate()
     {
         return date;
+
     }
+
 }
