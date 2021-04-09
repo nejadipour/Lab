9a2ef409ccc350b243eba8329d7c9175cfe496b0
Alireza Nejadipour <78561069+nejadipour@users.noreply.github.com>
2021-04-09 22:56:51 +0430

    refactor: code cleaned and Javadoc added

--- session04/Voting.java
+++ session04/Voting.java
@@ -8,16 +8,23 @@ import java.util.GregorianCalendar;
  * this class models a voting
  * it contains a question and choices
  * the votes will be stored here
+ * @version 2.4
+ * @author Alireza Nejadipour
  */
 
 public class Voting
 {
     // type shows if the vote is single(0) or multiple(1)
-    private int type;
-    private String question;
-    private ArrayList<Person> voters;
-    private HashMap<String, HashSet<Vote>> listOfVotesToChoice; // key: choice, value: vote
+    private final int type;
+    private final String question;
+    private final ArrayList<Person> voters;
+    private final HashMap<String, HashSet<Vote>> listOfVotesToChoice; // key: choice, value: vote
 
+    /**
+     * create a new voting
+     * @param type it is single or multiple
+     * @param question the question of the voting
+     */
     public Voting(int type, String question)
     {
         this.type = type;
@@ -39,6 +46,11 @@ public class Voting
     }
 
 
+    /**
+     * adds a new vote to the list
+     * @param person who has voted!
+     * @param choices which choices the voter has decided
+     */
     public void vote(Person person, ArrayList<String> choices)
     {
         GregorianCalendar gregorianCalendar = new GregorianCalendar();
@@ -47,7 +59,6 @@ public class Voting
         voters.add(person);
         for (String choice : choices)
         {
-            //listOfVotesToChoice.putIfAbsent(choice, new HashSet<>());
             Vote newVote = new Vote(person, jalal.toString());
             listOfVotesToChoice.get(choice).add(newVote);
 
@@ -56,6 +67,11 @@ public class Voting
     }
 
 
+    /**
+     * checks if the voter has voted before or not
+     * @param person the person that should be checked
+     * @return if it is new returns false
+     */
     public boolean personExist(Person person)
     {
         for (Person personToCheck : voters)
@@ -73,31 +89,54 @@ public class Voting
     }
 
 
+    /**
+     * gets list of voters of this voting
+     * @return field voters is returned
+     */
     public ArrayList<Person> getVoters()
     {
         return voters;
+
     }
 
+    /**
+     * gets the question of the voting
+     * @return field question is returned
+     */
     public String getQuestion()
     {
         return question;
+
     }
 
+    /**
+     * gets choices of the voting
+     * @return field choices is returned
+     */
     public ArrayList<String> getChoices()
     {
         return new ArrayList<>(listOfVotesToChoice.keySet());
 
     }
 
+    /**
+     * gets the type of voting
+     * @return type field is returned
+     */
     public int getType()
     {
         return type;
 
     }
 
+    /**
+     * gets list of choices with list of each choice's voters
+     * @return listOfVotesToChoice is returned
+     */
     public HashMap<String, HashSet<Vote>> getListOfVotesToChoice()
     {
         return listOfVotesToChoice;
 
     }
+    
 }
