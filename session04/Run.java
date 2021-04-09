66074e8bdec9f27353d4c30f4ff593c8cbd6d878
Alireza Nejadipour <78561069+nejadipour@users.noreply.github.com>
2021-04-09 19:13:39 +0430

    feat: voting is available now!

--- session04/Run.java
+++ session04/Run.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main
@@ -6,6 +7,7 @@ public class Main
     private Scanner scanner;
     private String mainMeu;
     private String typeMenu;
+    private VotingSystem runtimeSystem;
 
 
     public Main()
@@ -13,8 +15,8 @@ public class Main
         mainMeu =
                 """
                     1.new voting
-                    2.results
-                    3.show all voting
+                    2.vote
+                    3.results
                         """;
 
         typeMenu =
@@ -24,6 +26,8 @@ public class Main
                         """;
 
         scanner = new Scanner(System.in).useDelimiter("\n");
+
+        runtimeSystem = new VotingSystem();
 
     }
 
@@ -36,9 +40,20 @@ public class Main
 
         switch (choice)
         {
-            case 1 -> new_voting();
-            case 2 -> results();
+            case 1 ->
+            {
+                newVoting();
+                printMainMenu();
+            }
+            case 2 ->
+            {
+                vote();
+                printMainMenu();
+            }
+            case 3 -> results();
+
         }
+
     }
 
 
@@ -51,18 +66,20 @@ public class Main
     }
 
 
-    public void new_voting()
+    public void newVoting()
     {
         System.out.print("Enter the question : ");
         String question = scanner.next();
-        
+
         System.out.println("Enter the questions : ");
         System.out.println("(when it is finished enter \"finish\")");
-        
+
         ArrayList<String>choices = new ArrayList<>();
         String choiceToAdd;
+        int choiceNum = 1;
         while (true)
         {
+            System.out.print(choiceNum + ". ");
             choiceToAdd = scanner.next();
 
             if (choiceToAdd.equals("finish"))
@@ -70,14 +87,98 @@ public class Main
 
             choices.add(choiceToAdd);
 
+            choiceNum++;
+
         }
 
         System.out.println("Choose your type :");
         System.out.println(typeMenu);
-        int type = scanner.nextInt() - 1;
-        
+        userChoice();
+
+        runtimeSystem.createVoting(question, choice - 1, choices);
+
+        System.out.println("voting added.");
 
     }
+
+
+    public void vote()
+    {
+        if(runtimeSystem.printVotingQuestions())
+        {
+            userChoice();
+
+            Voting voting = runtimeSystem.getVotingList().get(choice - 1);
+
+            System.out.print("Enter your first name : ");
+            String firstName = scanner.next();
+            System.out.print("Enter your last name : ");
+            String lastName = scanner.next();
+            Person person = new Person(firstName, lastName);
+
+            if (voting.personExist(person))
+            {
+                System.out.println("You have voted before.");
+                return;
+
+            }
+
+            runtimeSystem.printVoting(choice);
+
+
+            int type = voting.getType();
+            ArrayList<String> choices = new ArrayList<>();
+
+
+
+
+
+            if (type == 0)
+            {
+                System.out.print("Enter your choice(just one) : ");
+                int singleChoice = scanner.nextInt();
+                choices.add(voting.getChoices().get(singleChoice - 1));
+
+            }
+            else
+            {
+                System.out.println("Enter your choices(U can have more than one choice) : ");
+                System.out.println("When it is finished enter \"finish\"");
+
+                ArrayList<Integer> choicesArr = new ArrayList<>();
+
+                while (true)
+                {
+                    System.out.print("Your choice : ");
+                    String inp = scanner.next();
+
+                    if (inp.equals("finish"))
+                    {
+                        break;
+
+                    }
+                    choicesArr.add(Integer.parseInt(inp));
+
+                }
+
+                for (int intChoice : choicesArr)
+                {
+                    choices.add(voting.getChoices().get(intChoice - 1));
+
+                }
+
+            }
+
+            runtimeSystem.vote(choice - 1, person, choices);
+            
+        }
+
+
+
+
+
+    }
+
 
 
     public void results()
@@ -91,10 +192,10 @@ public class Main
 
     public static void main(String[] args)
     {
-
         Main run = new Main();
 
         run.printMainMenu();
 
     }
+
 }
