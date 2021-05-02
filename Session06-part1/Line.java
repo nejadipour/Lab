680d42c813b6c5c5c62627070fb5f7e1350dbc55
Alireza Nejadipour <78561069+nejadipour@users.noreply.github.com>
2021-05-02 10:59:41 +0430

    feat: Line class added

---
+++ Session06-part1/Line.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+
+public class Line
+{
+    private ArrayList<Seat> seats;
+
+    public Line(ArrayList<Seat> seats) {
+        this.seats = seats;
+    }
+
+    public void findEmptySeats()
+    {}
+
+}
