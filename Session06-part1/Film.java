c8f35d93574389843ccb5b2a34dd51a08b9dbe6b
Alireza Nejadipour <78561069+nejadipour@users.noreply.github.com>
2021-05-02 11:00:12 +0430

    feat: Film class added

---
+++ Session06-part1/Film.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+
+public class Film
+{
+    private String name;
+    private ArrayList<String> dates;
+
+    public Film(String name, ArrayList<String> dates)
+    {
+        this.name = name;
+        this.dates = dates;
+
+    }
+
+    public void addDate(String date)
+    {}
+
+}
